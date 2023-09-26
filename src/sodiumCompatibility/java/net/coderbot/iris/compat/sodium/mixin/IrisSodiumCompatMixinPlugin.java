5b6e1f7337497dece56559de06f2a3d4a8e23d9f
CreativeMD <Creative-Multimedia-Design@gmx.de>
2023-09-26 14:37:17 +0200

    Added support for all different versions of sodium

--- src/sodiumCompatibility/java/net/coderbot/iris/compat/sodium/mixin/IrisSodiumCompatMixinPlugin.java
+++ src/sodiumCompatibility/java/net/coderbot/iris/compat/sodium/mixin/IrisSodiumCompatMixinPlugin.java
@@ -1,58 +1,67 @@
 package net.coderbot.iris.compat.sodium.mixin;
 
-import net.fabricmc.loader.api.FabricLoader;
+import java.util.List;
+import java.util.Set;
+
 import org.objectweb.asm.tree.ClassNode;
 import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
 import org.spongepowered.asm.mixin.extensibility.IMixinInfo;
 
-import java.util.List;
-import java.util.Set;
+import net.minecraftforge.fml.ModList;
 
-/**
- * Semi-critical mixin config plugin, disables mixins if Sodium isn't present,
+/** Semi-critical mixin config plugin, disables mixins if Sodium isn't present,
  * since on 1.18+ we have mixins into Iris classes that crash the game instead of just
- * spamming the log if Sodium isn't present.
- */
+ * spamming the log if Sodium isn't present. */
 public class IrisSodiumCompatMixinPlugin implements IMixinConfigPlugin {
+
+	public static String[] SODIUM_PORTS = new String[] { "rubidium", "embeddium", "sodiumforged" };
 	private boolean validSodiumVersion = false;
 
+	private static boolean isInstalled() {
+		var list = ModList.get();
+		for (int i = 0; i < SODIUM_PORTS.length; i++)
+			if (list.isLoaded(SODIUM_PORTS[i]))
+				return true;
+		return false;
+	}
+	
 	@Override
 	public void onLoad(String mixinPackage) {
-		validSodiumVersion = FabricLoader.getInstance().isModLoaded("sodium");
-
+		validSodiumVersion = isInstalled();
+		
 		if (!validSodiumVersion) {
 			// We can't use Iris' logger here due to classloading issues.
 			System.err.println("[Iris] Invalid/missing version of Sodium detected, disabling compatibility mixins!");
 		}
 	}
-
+	
 	@Override
 	public String getRefMapperConfig() {
 		return null;
 	}
-
+	
 	@Override
 	public boolean shouldApplyMixin(String targetClassName, String mixinClassName) {
 		return validSodiumVersion;
 	}
-
+	
 	@Override
 	public void acceptTargets(Set<String> myTargets, Set<String> otherTargets) {
-
+		
 	}
-
+	
 	@Override
 	public List<String> getMixins() {
 		return null;
 	}
-
+	
 	@Override
 	public void preApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {
-
+		
 	}
-
+	
 	@Override
 	public void postApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {
-
+		
 	}
 }
